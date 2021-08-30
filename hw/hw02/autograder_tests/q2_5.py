c9a1b20179a9f8b9d78677e7db41b314260bb6a7
jhb10c <jhb10c@my.fsu.edu>
2021-08-30 18:25:51 -0400

    updated test files

--- hw/hw02/autograder_tests/q2_5.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q2_5',
-    'points': 1,
-    'suites': [   {   'cases': [   {   'code': '>>> sum_of_birth_years\n5433',
-                                       'hidden': False,
-                                       'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
