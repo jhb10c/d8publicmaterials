ccf371973c0bc98a6910de062da283e58dcc3a5d
jhb10c <jhb10c@my.fsu.edu>
2021-09-14 00:33:08 -0400

    update homework 3

---
+++ hw/hw03/tests/q4_5.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q4_4',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> 1 <= answer_here <= '
+                                               '4\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
