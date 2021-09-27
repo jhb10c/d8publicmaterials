84bc7d28c4c696cb17ede87e12f9f74ef57059de
jhb10c <jhb10c@my.fsu.edu>
2021-09-27 12:37:22 -0400

    update lab 4

---
+++ hw/hw04/tests/q1_3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q1_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> pattern == True
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
