7b6e3bb7414544f5a89c0dad7693c4bf47aca457
jhb10c <jhb10c@my.fsu.edu>
2021-10-26 09:50:30 -0400

    updat16

---
+++ hw/hw06/tests/q5_5.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q5_4',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> model_answer < 3',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
